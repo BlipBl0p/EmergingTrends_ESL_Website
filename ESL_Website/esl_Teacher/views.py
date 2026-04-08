a1880f4c04f3623bb1005d47a7581d823262a89f
Bakunawa0 <danieljuliandado4@gmail.com>
2026-04-08 17:48:03 +0800

    teacher dash shows username

--- ESL_Website/esl_Teacher/views.py
+++ ESL_Website/esl_Teacher/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def t_dashboard(request):
     if request.user.is_authenticated:
-        return render(request, 'pages/teacher_dashboard.html')
+        print('authenticated')
+        name = request.user.username
+        info = {
+            "username": name,
+        }
+        return render(request, 'pages/teacher_dashboard.html', info)
     return redirect('/login')
