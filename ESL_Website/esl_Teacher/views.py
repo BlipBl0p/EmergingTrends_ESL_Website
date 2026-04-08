353f2892101f8f40395c5b46164969dde42b45fc
Bakunawa0 <danieljuliandado4@gmail.com>
2026-04-08 18:20:23 +0800

    date appears in the teacher dash

--- ESL_Website/esl_Teacher/views.py
+++ ESL_Website/esl_Teacher/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
+import datetime
 
 # Create your views here.
 def t_dashboard(request):
     if request.user.is_authenticated:
-        print('authenticated')
         name = request.user.username
+        today = datetime.datetime.now().strftime("%b %e")
         info = {
             "username": name,
+            "today": today
         }
         return render(request, 'pages/teacher_dashboard.html', info)
     return redirect('/login')
